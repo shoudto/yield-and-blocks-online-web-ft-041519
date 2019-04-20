8537721b428c0a919dc67b41ec0f7feb3b751986
Oscar Shoudt <shoudto@gmail.com>
2019-04-20 23:51:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,11 +5,12 @@ def hello_t(array)
   if block_given?
     yield array[i]
     i = i + 1  
- end 
-  array 
-else 
-  puts "Hey! No block was given!"
-end 
+    end 
+    
+   array 
+  else 
+   puts "Hey! No block was given!"
+  end 
 end
 
 # call your method here!
